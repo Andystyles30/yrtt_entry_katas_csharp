51439b628260f55ae20d8ec1c503424dca92a837
Andy Styles <Styles@caytonslaw.com>
2020-11-07 14:45:11 +0000

    implement and pass tests for exercise 004

--- Tasks/Exercise004.cs
+++ Tasks/Exercise004.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
 // Leave punctuation marks untouched.
@@ -9,7 +12,13 @@ namespace TechReturners.Tasks
     {
         public static string PigIt(string str)
         {
-            throw new NotImplementedException();
+            List<string> words = new List<string>();
+            foreach (string word in str.Split(' ')) {
+                string removePunctuation = Regex.Replace(word, @"[^\w\s]", "");
+                string newWord = word.Replace(removePunctuation, string.Join("", removePunctuation.Skip(1)) + removePunctuation[0] + "ay");
+                words.Add(newWord);
+            }
+            return string.Join(" ", words);
         }
     }
 }
